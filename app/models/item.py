1bd838503f80a145f461e61519291e99f78e461c
Sergey Martynov <acemore007@gmail.com>
2025-10-16 14:33:22 +0300

    Add item models

---
+++ app/models/item.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+
+
+class ItemUpdate(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
